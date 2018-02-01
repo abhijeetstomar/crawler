781210f466c0f76d68404ce55e3b2a6bd8f01845
abhijeetstomar <contact.abhijeetsingh@gmail.com>
2018-02-02 01:27:47 +0530

    Added main function

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # crawler
-Recursive web crawler
+
+Web crawler to recursively extract all the links reachable from a seed webpage.
+
+To use:
+1. Run the script crawler.py using python3
+2. Paste link of seed webpage
+3. Hit Enter
--- crawler.py
+++ crawler.py
@@ -1,12 +1,19 @@
-# Web crawler to recursively extract all the links from a webpage
+# Web crawler to recursively extract all the links reachable from a seed webpage
+# This web crawler follows depth first search for finding links.
 # Created by Abhijeet Singh. 01-Feb-2018.
+# Testcase: https://xkcd.com/353/
+# Example: seed = 'https://udacity.github.io/cs101x/index.html'
 
+seed = input()
+
+# get source text url as return value
 def get_page(url): 
 	try: 
 		import urllib.request
 		return urllib.request.urlopen(url).read().decode("utf8")
 	except: return
 
+# return link url and get position of next link
 def get_next_target(page):
 	start_link = page.find('<a href=')
 
@@ -21,6 +28,7 @@ def get_next_target(page):
 	# [a:b] returns index a to b-1
 	return url, end_quote
 
+# return all links in a webpage as list
 def get_all_links(page):
 	links = []
 	while True:
@@ -32,8 +40,28 @@ def get_all_links(page):
 			break
 	return links
 
-get_all_links(get_page('https://udacity.github.io/cs101x/index.html'))
-# https://xkcd.com/353/
+# add disjoint elements of q to p
+def union(p,q):
+	for i in q:
+		if i not in p:
+			p.append(i)
 
-tocrawl = [] # list of pages left to crawl
-crawled = [] # list of pages crawled
+# crawl seed page for links recursively
+def crawl_web(seed):
+	tocrawl = [seed] # list of pages left to crawl
+	crawled = [] # list of pages crawled
+
+	while tocrawl:
+		page = tocrawl.pop()
+		# we only crawl the page if we haven't already
+		if page not in crawled:
+			union(tocrawl, get_all_links(get_page(page)))
+			crawled.append(page)
+	
+	return crawled
+
+# Main program
+print("\nReachable links:")
+crawled = crawl_web(seed)
+for i in crawled:
+	print(i)
